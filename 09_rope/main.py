5a31834650a2ea2f639c49e6f74b3b3762276a45
Nestor Morales <nestormorales1998@gmail.com>
2022-12-09 09:41:46 +0100

    simplification

--- 09_rope/main.py
+++ 09_rope/main.py
@@ -16,21 +16,19 @@ LETTER_TO_DIRECTION = {
 }
 
 
-def get_tail_visit_of_rope(input: List[str], length: int) -> int:
+def simulate_rope_and_get_tail_visit(movements: List[str], length: int) -> int:
 
     start = (0, 0)
+    rope = [start] * length
     visited_by_tail = [start]
-    rope = visited_by_tail * length
-    hx, hy = rope[0]
-    tx, ty = rope[-1]
 
-    for instruction in input:
-        direction, value = instruction.split()
+    for movement in movements:
+        direction, value = movement.split()
         dx, dy = LETTER_TO_DIRECTION[direction]
         for _ in range(int(value)):
-            # udpate head 
+            # udpate head
             hx, hy = rope[0]
-            rope[0] = hx+dx, hy+dy
+            rope[0] = hx + dx, hy + dy
             # simulate tail concat movement
             for i in range(length - 1):
 
@@ -42,23 +40,23 @@ def get_tail_visit_of_rope(input: List[str], length: int) -> int:
                 diff_x = hx - tx
                 diff_y = hy - ty
 
-                # tail shouldn't move
+                # if is touching, do nothing
                 if abs(diff_x) <= 1 and abs(diff_y) <= 1:
                     continue
 
                 # update tail
-                tx += 0 if diff_x == 0 else diff_x // abs(diff_x) # +1, 0, -1
-                ty += 0 if diff_y == 0 else diff_y // abs(diff_y) # +1, 0, -1
-                rope[i+1] = tx, ty
+                tx += 0 if diff_x == 0 else diff_x // abs(diff_x)  # +1, 0, -1
+                ty += 0 if diff_y == 0 else diff_y // abs(diff_y)  # +1, 0, -1
+                rope[i + 1] = tx, ty
 
             visited_by_tail.append(rope[-1])  # only append final tail
     return len(set(visited_by_tail))  # return unique visited
 
 
 def main():
-    movement_instructions = read_input_as_lines('input.txt')
-    rope_of_two = get_tail_visit_of_rope(movement_instructions, 2)
-    rope_of_ten = get_tail_visit_of_rope(movement_instructions, 10)
+    movements = read_input_as_lines('input.txt')
+    rope_of_two = simulate_rope_and_get_tail_visit(movements, 2)
+    rope_of_ten = simulate_rope_and_get_tail_visit(movements, 10)
     print(rope_of_two)
     print(rope_of_ten)
 
